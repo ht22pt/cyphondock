29a87712ef32826b1895c73b39fb4bdf9df1eac2
David Gidwani <david.gidwani@gmail.com>
2017-06-27 12:27:01 -0400

    use bridge ip for postgres service rather than ip of container

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -5,6 +5,10 @@ docker exec cyphondock_cyphon_1 python manage.py migrate contenttypes || true
 bridge="$(docker exec cyphondock_cyphon_1 route -n | grep -Po '172\.\d+\.0\.1' | head -n1)"
 echo $bridge
 docker exec cyphondock_cyphon_1 route -n
+sed -ie "s/POSTGRES_HOST=.*/POSTGRES_HOST=${bridge}/" config-COPYME/env/cyphon.env
+docker-compose -f docker-compose.travis.yml stop cyphon
+docker-compose -f docker-compose.travis.yml up -d cyphon
+sleep 10
 docker exec cyphondock_cyphon_1 sed -ie "s/localhost/${bridge}/" tests/functional_tests.py
 docker exec cyphondock_cyphon_1 grep "${bridge}" tests/functional_tests.py
 # Make sure starter fixtures can load successfully and all tests pass.
