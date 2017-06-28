0bf36c07a4b4573717473b4feaf3c3aed596dd72
David Gidwani <david.gidwani@gmail.com>
2017-06-28 09:24:28 -0400

    attempting to use saucelabs connect container again...

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -ev
-netstat -4lnt
+# netstat -4lnt
 docker exec cyphondock_cyphon_1 python manage.py migrate contenttypes || true
-bridge="$(docker exec cyphondock_cyphon_1 route -n | grep -Po '172\.\d+\.0\.1' | head -n1)"
-echo $bridge
-docker exec cyphondock_cyphon_1 route -n
-sed -ie "s/POSTGRES_HOST=.*/POSTGRES_HOST=${bridge}/" config-COPYME/env/cyphon.env
-docker-compose -f docker-compose.travis.yml stop cyphon
-docker-compose -f docker-compose.travis.yml up -d cyphon
-sleep 10
-docker exec cyphondock_cyphon_1 sed -ie "s/localhost/${bridge}/" tests/functional_tests.py
-docker exec cyphondock_cyphon_1 grep "${bridge}" tests/functional_tests.py
+# bridge="$(docker exec cyphondock_cyphon_1 route -n | grep -Po '172\.\d+\.0\.1' | head -n1)"
+# echo $bridge
+# docker exec cyphondock_cyphon_1 route -n
+# sed -ie "s/POSTGRES_HOST=.*/POSTGRES_HOST=${bridge}/" config-COPYME/env/cyphon.env
+# docker-compose -f docker-compose.travis.yml stop cyphon
+# docker-compose -f docker-compose.travis.yml up -d cyphon
+# sleep 10
+docker exec cyphondock_cyphon_1 sed -ie "s/localhost/saucelabs/" tests/functional_tests.py
+docker exec cyphondock_cyphon_1 grep "saucelabs" tests/functional_tests.py
 # Make sure starter fixtures can load successfully and all tests pass.
 # Run tests with --keepdb to avoid OperationalError during teardown, in case
 # any db connections are stillr open from threads in TransactionTestCases.
