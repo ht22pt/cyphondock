b1caa1bbeeda95cde967cca1334e48ea4d2ffc34
David Gidwani <david.gidwani@gmail.com>
2017-06-28 12:11:10 -0400

    attempting to reproduce selenium errors in travis

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -15,32 +15,9 @@ docker exec cyphondock_cyphon_1 grep "saucelabs" tests/functional_tests.py
 # Run tests with --keepdb to avoid OperationalError during teardown, in case
 # any db connections are stillr open from threads in TransactionTestCases.
 docker exec cyphondock_cyphon_1 python manage.py loaddata fixtures/starter-fixtures.json || true
-while ! docker logs cyphondock_saucelabs_1 | grep -q "Selenium listener started"
-do
-  echo "Waiting for Saucelabs Connect proxy..."
-  sleep 10
-done
-docker exec -it \
-  -e FUNCTIONAL_TESTS_DRIVER=SAUCELABS \
-  -e SAUCE_USERNAME \
-  -e SAUCE_ACCESS_KEY \
-  -e TRAVIS_JOB_NUMBER \
-  -e TRAVIS_BUILD_NUMBER \
-  -e TRAVIS_PYTHON_VERSION \
-  -e TWITTER_ACCESS_TOKEN \
-  -e TWITTER_KEY \
-  -e TWITTER_SECRET \
-  -e TWITTER_TOKEN_SECRET \
-  cyphondock_cyphon_1 python -c "from tests.functional_tests import *; print(get_web_driver())"
-docker exec \
-  -e FUNCTIONAL_TESTS_DRIVER=SAUCELABS \
-  -e SAUCE_USERNAME \
-  -e SAUCE_ACCESS_KEY \
-  -e TRAVIS_JOB_NUMBER \
-  -e TRAVIS_BUILD_NUMBER \
-  -e TRAVIS_PYTHON_VERSION \
-  -e TWITTER_ACCESS_TOKEN \
-  -e TWITTER_KEY \
-  -e TWITTER_SECRET \
-  -e TWITTER_TOKEN_SECRET \
-  cyphondock_cyphon_1 python manage.py test -p test_functional.py --noinput --keepdb -v 2
+# while ! docker logs cyphondock_saucelabs_1 | grep -q "Selenium listener started"
+# do
+#   echo "Waiting for Saucelabs Connect proxy..."
+#   sleep 10
+# done
+docker exec cyphondock_cyphon_1 python manage.py test -p test_functional.py --noinput --keepdb -v 2
